75e854efef1845d6fea066867100b4b29d2e544a
Varun Negi <varunnegiweb@gmail.com>
2026-05-23 00:10:54 +0530

    Fixed CORS for production

--- Backend/server.js
+++ Backend/server.js
@@ -4,40 +4,24 @@ import cors from "cors";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import chatRoutes from "./routes/chatRoutes.js";
-
 import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ai-saas-chatbot-3t44stgb7-varun-saas-projects.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "AI SaaS API Running",
-  });
-});
-
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
