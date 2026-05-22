e84cee9357f88669dc80edebfa45075bf4d87f6e
Varun Negi <varunnegiweb@gmail.com>
2026-05-23 00:20:03 +0530

    Fixed final CORS issue

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+
 import chatRoutes from "./routes/chatRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
@@ -16,6 +17,7 @@ app.use(
       "http://localhost:5173",
       "https://ai-saas-chatbot-3t44stgb7-varun-saas-projects.vercel.app",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
